6275d67b8676f0f504ee760824d8fd9d919ce816
Ailea Patrinely <aileapatrinely@gmail.com>
2020-07-01 13:44:25 -0500

    fixes bug shows artist array  on dom

--- server/public/client.js
+++ server/public/client.js
@@ -1,21 +1,23 @@
 $(document).ready(onReady);
 
+let artist = [];
+
 function onReady() {
-    $.ajax({
-        type: 'GET',
-        url: '/artist'
-    }).then(function (response) {
-        for (let i = 0; i < response.length; i++) {
-            let artist = response[i];
-            $('#artistTableBody').append(`
+  $.ajax({
+    type: 'GET',
+    url: '/artist',
+  }).then(function (response) {
+    artist = response;
+    for (let info of artist) {
+      $('#artistTableBody').append(`
                 <tr>
                     <td>${artist.name}</td>
                     <td>${artist.born}</td>
                     <td>${artist.died}</td>
                 </tr>
             `);
-        }
-    });
+    }
+  });
 
-    // TODO Add ajax request for /songs and display on DOM
+  // TODO Add ajax request for /songs and display on DOM
 }
